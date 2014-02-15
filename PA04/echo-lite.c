303425e8d35bba786711a2d7d84016954a588e50
Myles <mcasanas@purdue.edu>
2014-02-14 19:13:39 -0500

    testing

---
+++ PA04/echo-lite.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char ** argv)
+{
+  int i;
+  for(i = 1; i < argc; i++)
+  {
+    //if(*argv[i] =) printf(" ");
+    printf("%s", argv[i]);
+    
+    if (i != argc - 1) 
+    {
+      printf(" ");
+    }
+  }
+
+
+  printf("\n");
+  return EXIT_SUCCESS;
+}
