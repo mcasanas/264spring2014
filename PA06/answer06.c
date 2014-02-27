644c7d37ca947ad0c2249598212e935b69b48aa7
Myles <mcasanas@purdue.edu>
2014-02-26 21:17:03 -0500

    increase and decrease done, fix by generating w/o all partitions

--- PA06/answer06.c
+++ PA06/answer06.c
@@ -38,20 +38,35 @@ void printPartition(int *partition, int length)
 
 int increaseCheck(int *array, int pos)
 {
-  printf("%d    ",pos);
-  if(pos == 0) {
+  //printf("%d    ",pos);
+  if(pos < 1){
     return 1;
   }  
-
   int ind;
-  for (ind = 1; ind <= pos; ind++)
+  for (ind = 1; ind < pos; ind++)
   {
-    printf("\n %d and %d\n", array[ind-1], array[ind]);
-    if(array[ind - 1] >  array[ind]) {
+    //printf("\n %d and %d\n", array[ind-1], array[ind]);
+    if(array[ind - 1] > array[ind]) {
       return 0;
     }
   }
+  return 1;
+}
 
+int decreaseCheck(int *array, int pos)
+{
+  //printf("%d    ",pos);
+  if(pos < 1){
+    return 1;
+  }  
+  int ind;
+  for (ind = 1; ind < pos; ind++)
+  {
+    //printf("\n %d and %d\n", array[ind-1], array[ind]);
+    if(array[ind - 1] < array[ind]) {
+      return 0;
+    }
+  }
   return 1;
 }
 
@@ -61,21 +76,37 @@ void partitionIncreasingHelper(int a, int *array, int pos)
   if(a == 0)
   {
     //printf("\n %d\n", increaseCheck(array,pos));
-    if(increaseCheck(array, pos)) 
-    {
-      
+    if(increaseCheck(array, pos)){ 
       printPartition(array, pos);
     }  
   return;
   }
 
+  int b;
+  for(b = 1; b <= a; b++)
+  {
+    array[pos] = b;
+    partitionIncreasingHelper(a - b, array, pos + 1);    
+  }
+}
+
+void partitionDecreasingHelper(int a, int *array, int pos)
+{
+  if(a == 0)
+  {
+    //printf("\n %d\n", increaseCheck(array,pos));
+    if(decreaseCheck(array, pos)) 
+    { 
+      printPartition(array, pos);
+    }  
+  return;
+  }
 
   int b;
   for(b = 1; b <= a; b++)
   {
     array[pos] = b;
-    partitionIncreasingHelper(a - b, array, pos + 1);
-    
+    partitionDecreasingHelper(a - b, array, pos + 1);    
   }
 }
 
@@ -96,5 +127,12 @@ void partitionIncreasing(int value)
 {
   int *array = malloc(sizeof(int) * value);
   partitionIncreasingHelper(value, array, 0);
+  free(array);
+}
+
+void partitionDecreasing(int value)
+{
+  int *array = malloc(sizeof(int) * value);
+  partitionDecreasingHelper(value, array, 0);
   free(array);
 }
