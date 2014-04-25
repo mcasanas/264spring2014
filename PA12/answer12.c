8326f34b4ed253ce0e0b36f37171049d1376aa45
Live session user <mcasanas@purdue.edu>
2014-04-25 09:19:54 -0400

    end?

--- PA12/answer12.c
+++ PA12/answer12.c
@@ -60,11 +60,46 @@ void *check_prime(void *arg)
       if((n%i)==0) *(task->is_prime) = 0;
     }
   }
+  pthread_exit(0);
 }
 
 int primalityTestParallel(uint128 value, int n_threads)
 {
+  if (value <= 2) return 1;
+  if((value > 2) && (value % 2 == 0)) return 0;
+  uint128 max = floor(sqrt(value));
+  uint128 task_size = (max + (uint128) n_threads + (uint128) 1) / (uint128) n_threads;
+   
+  pthread_t *threads = malloc(sizeof(pthread_t) * n_threads);
+  long *status = malloc(sizeof(long) * n_threads);
+  Task *tasks = malloc(sizeof(Task) * n_threads);
   
+  int is_prime = TRUE;
+  int i, check;
+  
+  for(i = 0; i < n_threads; i++){
+    tasks[i].n = value;
+    tasks[i].start_n = task_size * (uint128) i;
+    tasks[i].stop_n = task_size * (uint128)(i+1);
+    tasks[i].is_prime = &is_prime;
+  }
+  
+  for(i = 0; i < n_threads; i++){
+    check = pthread_create(&(threads[i]), NULL, check_prime, (void*) &(tasks[i]));
+    if(check){
+      fprintf(stderr, "failed to create thread %d\n", i);
+    }
+  }
+
+  for(i = 0; i < n_threads; i++){
+    pthread_join(threads[i], (void*) &(status[i]));
+  }
+  
+  free(threads);
+  free(status);
+  free(tasks);
+  return is_prime;
+
 }
 /*
 int genRandomArray(int number)
@@ -89,6 +124,8 @@ int main(int argc, char * * argv)
     qsort(array,val,sizeof(int), intcmp);
   }
   */
+
+  //conversions
     const char * str = "340282366920938463463374607431768211455";
     uint128 w = alphaTou128(str);
     char * w_str = u128ToString(w);
@@ -98,4 +135,14 @@ int main(int argc, char * * argv)
     free(w_str);
     return EXIT_SUCCESS;
   
+
+    //primes
+    if(argc != 3) return EXIT_FAILURE;
+    char *num = argv[2];
+    int n_threads = atoi(argv[1]);
+    uint128 value = alphaTou128(num);
+    int is_prime(primalityTestParallel(value, n_threads));
+    return EXIT_SUCCESS;
+    
+    
 }
